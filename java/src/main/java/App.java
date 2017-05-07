51a9c7c6444c4b5003ed0a3a0aa3b526db4a0608
Budzin <kontakt@mbudzinski.pl>
2017-05-07 18:56:01 +0200

    Initial WiP

--- java/src/main/java/App.java
+++ java/src/main/java/App.java
@@ -1,7 +1,21 @@
+import java.math.BigInteger;
+import java.util.Iterator;
+
 public class App {
 
     public static void main(String[] args) {
         FibonacciSequence sequence = new FibonacciSequence();
+
+        Iterator<BigInteger> fibonacciIterator = sequence.iterator();
+        for (int i = 0; i < 51; i ++) {
+            System.out.println(i + ": " + fibonacciIterator.next());
+        }
+
+        sequence.setPointer(10000000);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + fibonacciIterator.next());
+        }
 
     }
 }
