b0297b8e6cef3a451419ce7a0d121ef38e589681
Budzin <kontakt@mbudzinski.pl>
2017-05-07 18:56:01 +0200

    Initial WiP

--- java/src/main/java/App.java
+++ java/src/main/java/App.java
@@ -1,7 +1,19 @@
+import java.math.BigInteger;
+import java.util.Iterator;
+
 public class App {
 
     public static void main(String[] args) {
         FibonacciSequence sequence = new FibonacciSequence();
+
+        Iterator<BigInteger> fibonacciIterator = sequence.iterator();
+        for (int i = 0; i < 51; i ++) {
+            System.out.println(i + ": " + fibonacciIterator.next());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + fibonacciIterator.next());
+        }
 
     }
 }
--- java/src/main/java/FibonacciSequence.java
+++ java/src/main/java/FibonacciSequence.java
@@ -1,19 +1,96 @@
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 
-public class FibonacciSequence {
+public class FibonacciSequence implements Iterable<BigInteger>{
 
-    private List<BigDecimal> sequence = new ArrayList<>();
+    private List<BigInteger> sequence;
+
+    private int pointer = 0;
 
     public FibonacciSequence() {
-        sequence.add(BigDecimal.ZERO);
-        sequence.add(BigDecimal.ONE);
-        sequence.add(BigDecimal.ONE);
+        sequence = new ArrayList<>();
+        sequence.add(BigInteger.ZERO);
+        sequence.add(BigInteger.ONE);
+        sequence.add(BigInteger.ONE);
     }
 
-    private List<BigDecimal> getSequence() {
-        return sequence;
+
+    public Stream<BigInteger> getStream() {
+        return sequence.stream();
+    }
+
+    private BigInteger getLast() {
+        return sequence.get(sequence.size() - 1);
+    }
+
+    private BigInteger getValueBeforeLast() {
+        return sequence.get(sequence.size() - 2);
+    }
+
+    @Override
+    public Iterator<BigInteger> iterator() {
+
+        return new Iterator<BigInteger>() {
+
+            @Override
+            public boolean hasNext() {
+                return pointer <= sequence.size() - 1;
+            }
+
+            @Override
+            public BigInteger next() {
+                BigInteger valueToReturn;
+
+                if (hasNext()) {
+                    valueToReturn = sequence.get(pointer);
+                    ++pointer;
+                    return valueToReturn;
+                } else {
+                    while (!hasNext()) {
+                        BigInteger lastValue = getLast();
+                        sequence.add(lastValue.add(getValueBeforeLast()));
+                    }
+                }
+
+                valueToReturn = getLast();
+                ++pointer;
+                return valueToReturn;
+            }
+        };
+    }
+
+
+    @Override
+    public void forEach(Consumer<? super BigInteger> action) {
+        sequence.forEach(action::accept);
+    }
+
+    @Override
+    public Spliterator<BigInteger> spliterator() {
+
+        return new Spliterator<BigInteger>() {
+            @Override
+            public boolean tryAdvance(Consumer<? super BigInteger> action) {
+                return false;
+            }
+
+            @Override
+            public Spliterator<BigInteger> trySplit() {
+                return null;
+            }
+
+            @Override
+            public long estimateSize() {
+                return 0;
+            }
+
+            @Override
+            public int characteristics() {
+                return 0;
+            }
+        };
     }
 }
