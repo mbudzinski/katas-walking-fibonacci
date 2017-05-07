77be601fcc93b06587df9947ecdb52d95556c7a6
Budzin <kontakt@mbudzinski.pl>
2017-05-07 18:58:03 +0200

    Fix

--- java/src/main/java/App.java
+++ java/src/main/java/App.java
@@ -11,11 +11,6 @@ public class App {
             System.out.println(i + ": " + fibonacciIterator.next());
         }
 
-<<<<<<< HEAD
-=======
-        sequence.setPointer(10000000);
-
->>>>>>> 51a9c7c6444c4b5003ed0a3a0aa3b526db4a0608
         for (int i = 0; i < 10; i++) {
             System.out.println(i + ": " + fibonacciIterator.next());
         }
