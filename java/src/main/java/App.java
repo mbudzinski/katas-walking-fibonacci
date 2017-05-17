406a7596cf701d2ba5dfe43e6ee9d2741495918f
marcinbudzinski <marcin.budzinski@hrs.com>
2017-05-17 15:48:15 +0200

    Made use of fibonacci stream

--- java/src/main/java/App.java
+++ java/src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.math.BigInteger;
 import java.util.Iterator;
-import java.util.Spliterator;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class App {
 
@@ -15,5 +16,28 @@ public class App {
         for (int i = 0; i < 10; i++) {
             System.out.println(i + ": " + fibonacciIterator.next());
         }
+
+
+        FibonacciSequence sequenceToStream = new FibonacciSequence();
+
+        BigInteger three = new BigInteger("3");
+
+        for (int i = 0; i < 20; i++){
+            sequenceToStream.iterator().next();
+        }
+
+        Stream<BigInteger> divisibleByThree = sequenceToStream.getStream().filter(bigInteger ->
+                BigInteger.ZERO.equals(bigInteger.remainder(three)) && !BigInteger.ZERO.equals(bigInteger)
+        );
+
+        Optional<BigInteger> firstDivisibleByThree = divisibleByThree.findFirst();
+
+        if (firstDivisibleByThree.isPresent()) {
+            System.out.println("Found! : " + firstDivisibleByThree.get());
+        } else {
+            System.out.println("Not found!");
+        }
+
+
     }
 }
